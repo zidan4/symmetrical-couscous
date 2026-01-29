62ab4aee5ffe70aa56d1e472fc6ee5c2a2d062e7
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2026-01-29 19:25:06 +0300

    Add comments and a function in var_comments.lua

---
+++ var_comments.lua
@@ -0,0 +1,15 @@
+-- inline comment
+--[[
+  multiline comment
+  on several lines
+]]--
+
+message = 10
+local message = 8
+
+function find( foo )
+  local bar = foo
+  bar = bar / 2
+  return bar
+end
+  
